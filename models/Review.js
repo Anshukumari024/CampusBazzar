cf015a034c31fefa959ef01dd61c012f75060b56
Anshukumari024 <anshurajyadav.6639@gmail.com>
2025-08-23 00:50:22 +0530

    updated project files

--- models/Review.js
+++ models/Review.js
@@ -1,19 +1,18 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+    },
+    comment: {
+        type: String,
+        trim: true,
+    }
+},{timestamps:true});
 
 
-const mongoose =require("mongoose");
+const Review = mongoose.model('Review', reviewSchema);
 
-
-const reviewSchema=new mongoose.Schema({
-   rating:{
-    type:Number,
-    min:0,
-    max:5
-   },
-   comment:{
-    type:String,
-    trim:true
-   }
-},{timestamps:true})
-
-let Review =mongoose.model('Review',reviewSchema);
-module.exports=Review;
+module.exports = Review;
--- session-demo/app.js
+++ session-demo/app.js
@@ -1,38 +1,81 @@
-const express=require('express');
-const app=express();
-const session=require('express-session');
-app.use(session({
-    secret:'keyboard cat',
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const seedDB = require('./seed')
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override')
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/User');
+
+
+const productRoutes = require('./routes/product')
+const reviewRoutes = require('./routes/review')
+const authRoutes = require('./routes/auth')
+const cartRoutes = require('./routes/cart')
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/shopping-sam-app')
+.then(()=>{
+    console.log("DB connected successfully")
+})
+.catch((err)=>{
+    console.log("DB error"); 
+    console.log(err)
+})
+
+
+// session 
+let configSession = {
+    secret: 'keyboard cat',
     resave: false,
-    saveUninitialized: true
-}))
-
-app.get('/',(req,res)=>{
-    res.send('welcome to session');
-
-})
-
-app.get('/viewcount',(req,res)=>{
-    if(req.session.count){
-        req.session.count+=1;
+    saveUninitialized: true , 
+    cookie: { 
+        httpOnly: true ,
+        expires: Date.now() + 24*7*60*60*1000 , 
+        maxAge:24*7*60*60*1000
     }
-    else{
-        req.session.count=1;
-    }
-    res.send(`you visited ${req.session.count} times`)
+}
+
+app.engine('ejs' , ejsMate);
+app.set('view engine' , 'ejs');
+app.set('views' , path.join(__dirname , 'views')); // views folder 
+app.use(express.static(path.join(__dirname , 'public'))); // public folder
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+app.use(session(configSession)); 
+app.use(flash());
+// passport vaali
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req,res,next)=>{
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
 })
 
-app.get('/setname', (req,res)=>{
-    req.session.username='samarth vohra'
-    res.redirect('/greet')
-})
+// PASSPORT WAALI
+passport.use(new LocalStrategy(User.authenticate()));
 
-app.get('/greet',(req,res)=>{
-    let {username="anonymous"}=req.session;
-    res.send(`hi from ${username}`)
-})
+// seeding database
+// seedDB()
 
-app.listen(8080,()=>{
-    console.log('server  connected');
-    
+app.use(productRoutes); //so that harr incoming request ke liye path check kiya jaae
+app.use(reviewRoutes);  //so that harr incoming request ke liye path check kiya jaae
+app.use(authRoutes);  //so that harr incoming request ke liye path check kiya jaae
+app.use(cartRoutes);  //so that harr incoming request ke liye path check kiya jaae
+
+
+
+app.listen(8080 , ()=>{
+    console.log("server connected at port 8080")
 })
